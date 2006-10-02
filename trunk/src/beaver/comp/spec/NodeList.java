e66c869491acfa42e1a736c681814905018b97a9
alder <alder@709530b1-f071-48dc-a6d0-eebc4f786064>
2006-10-02 14:48:58 +0000

    file header

--- trunk/src/beaver/comp/spec/NodeList.java
+++ trunk/src/beaver/comp/spec/NodeList.java
@@ -1,3 +1,11 @@
+/***
+ * Beaver: compiler builder framework for Java                       
+ * Copyright (c) 2003-2006 Alexander Demenchuk <alder@softanvil.com>  
+ * All rights reserved.                       
+ *                          
+ * See the file "LICENSE" for the terms and conditions for copying,    
+ * distribution and modification of Beaver.                            
+ */
 package beaver.comp.spec;
 
 /**
